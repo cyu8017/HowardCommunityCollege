e1012229520f7ac249674a8c973a68fbca4a57a0
cyu8017 <96436370+cyu8017@users.noreply.github.com>
2023-05-07 12:33:47 -0400

    Fixed bug in display scores

--- CMSY-205__Advanced_JavaScript/ch05_assignment/test_scores/js/app.js
+++ CMSY-205__Advanced_JavaScript/ch05_assignment/test_scores/js/app.js
@@ -9,38 +9,38 @@
  */
 
 // Turn on strict mode.
-'use strict';
+"use strict"
 
 const app = () => {
-    const names = ["Ben", "Joel", "Judy", "Anne"];
-    const scores = [88, 98, 77, 88];
-    const n = 4;
+    const names = ["Ben", "Joel", "Judy", "Anne"]
+    const scores = [88, 98, 77, 88]
+    const n = 4
 
     /**
      * @name $
      * @desc Return output to HTML document with element of id.
      */
-    const $ = id => document.getElementById(id);
+    const $ = id => document.getElementById(id)
 
     /** 
      * @name addScore
      * @desc Add a full name and test score to the array.
      */
     function addScore() {
-        const name = $('name').value;
-        const score = parseInt($('score').value);
+        const name = $('name').value
+        const score = parseInt($('score').value)
 
         // Validate the user's entry.
         if (name == '' || isNaN(score) || score < 0 || score > 100) {
-            alert('You must enter a name and a valid score');
+            alert('You must enter a name and a valid score')
         } else {
-            names[names.length] = name;
-            scores[scores.length] = score;
-            $('name').value = '';
-            $('score').value = '';
+            names[names.length] = name
+            scores[scores.length] = score
+            $('name').value = ''
+            $('score').value = ''
         }
 
-        $('name').focus();
+        $('name').focus()
     }
 
     /**
@@ -48,36 +48,33 @@ const app = () => {
      * @desc Display the average high and low scores on the front end.
      */
     const displayResults = () => {
-        let high = scores[0];
-        let low = scores[0];
-        let avg = 0;
+        let high = scores[0]
+        let low = scores[0]
+        let avg = 0
+        let i
 
-        for (let i = 0; i < n; i++) {
+        for (i = 0; i < n; i++) {
             // High Score
-            if (high < scores[i]) {
-                high = scores[i];
-            }
-            high = high < scores[i] ? scores[i] : high;
+            if (high < scores[i]) high = scores[i]
+            
             // Low Score
-            if (low > scores[i]) {
-                low = scores[i];
-            }
-
+            if (low > scores[i]) low = scores[i]
+            
             // Average Score
-            avg = avg + scores[i];
+            avg = avg + scores[i]
         }
 
-        avg = avg / n;
+        avg = avg / n
 
         // Print average score, high score, low score to results id in HTML document.
-        let con = "Average Score = " + avg + "\nHigh Score = " + high + "\nLow Score = " + low + "\n";
+        let con = "Average Score = " + avg + "\nHigh Score = " + high + "\nLow Score = " + low + "\n"
 
-        for (let i = 0; i < scores.length; i++) {
-            con = con + "\n" + names[i] + ", " + scores[i];
+        for (i = 0; i < scores.length; i++) {
+            con = con + "\n" + names[i] + ", " + scores[i]
         }
 
-        $('results').innerHTML = con;
-    };
+        $('results').innerHTML = con
+    }
 
     /**
      * @name displayScores()
@@ -85,23 +82,23 @@ const app = () => {
      */
     const displayScores = () => {
         // Print all scores below. Show average, high, and low scores. 
-        let con = "<tr><td colspan='2'><h2>Scores</h2></td></tr><tr><td>Name</td><td>Score</td></tr>";
+        const con = "<tr><td colspan='2'><h2>Scores</h2></td></tr><tr><td>Name</td><td>Score</td></tr>"
 
-        for (let i = 0; i < n; i++) {
-            con = con + "<tr><td>" + names[i] + "</td><td>" + scores[i] + "</td></tr>";
+        for (i = 0; i < n; i++) {
+            con = con + "<tr><td>" + names[z] + "</td><td>" + scores[z] + "</td></tr>"
         }
 
-        $('results').innerHTML = con;
-    };
+        $('results').innerHTML = con
+    }
 
     /**
      * Document window onload
      * Apply action listeners to buttons.
      */
     window.onload = () => {
-        $('add').onclick = addScore;
-        $('display').onclick = displayResults;
-    };
-};
+        $('add').onclick = addScore
+        $('display').onclick = displayResults
+    }
+}
 
 app()
