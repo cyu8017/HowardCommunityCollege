6b163bc81726c6bf0fbef455be9b752e23263da1
Charlie Yu <cyu8017@gmail.com>
2023-07-17 11:10:12 -0400

    Updated lab 9 for optimizations and squashed defects.

--- CMSY-156_Introduction_to_Python/Winter_Semester_2021/lab09/lab09.py
+++ CMSY-156_Introduction_to_Python/Winter_Semester_2021/lab09/lab09.py
@@ -23,14 +23,14 @@ class Car:
     def getModel(self):
         return self.__model
 
-    def Accelerate(self):
+    def accelerate(self):
         if self.__speed <= 115:
             self.__speed += 5
         else:
             print("Warning: you cannot go faster than 120 MPH!!")
 
     def brake(self):
-        if(self.__speed >= 5):
+        if self.__speed >= 5:
             self.__speed -= 5
         else:
             print("Unable to brake, the vehicle is not moving!!")
@@ -38,43 +38,38 @@ class Car:
     def getSpeed(self):
         return self.__speed
 
-    def setspeed(self, speed):
+    def setSpeed(self, speed):
         self.__speed = speed
 
-# First Test Case
-obj1 = Car()
-for i in range(1, 6):
-    obj1.Accelerate()
-    print("The speed of the car after acceleration number ",
-          i, " is", obj1.getSpeed())
-print()
-for i in range(1, 6):
-    obj1.brake()
-    print("The speed of the car after brake number ", i, " is", obj1.getSpeed())
-print()
 
-# Second Test Case
-obj2 = Car()
-obj2.setspeed(100)
-for i in range(1, 6):
-    obj2.Accelerate()
-    print("The speed of the car after acceleration number ",
-          i, " is", obj2.getSpeed())
-print()
-for i in range(1, 6):
-    obj2.brake()
-    print("The speed of the car after brake number ", i, " is", obj2.getSpeed())
-print()
+def test_case(car):
+    for i in range(1, 6):
+        car.accelerate()
+        print("The speed of the car after acceleration number", i, "is", car.getSpeed())
 
-# Third Test Case
-obj3 = Car()
-obj3.setspeed(-5)
-for i in range(1, 6):
-    obj3.Accelerate()
-    print("The speed of the car after acceleration number ",
-          i, " is", obj3.getSpeed())
-print()
-for i in range(1, 6):
-    obj3.brake()
-    print("The speed of the car after brake number ", i, " is", obj3.getSpeed())
-print()
+    for i in range(1, 6):
+        car.brake()
+        print("The speed of the car after brake number", i, "is", car.getSpeed())
+
+
+def main():
+    # First Test Case:
+    obj1 = Car()
+    print("\nTest Case 1\n")
+    test_case(obj1)
+
+    # Second Test Case:
+    obj2 = Car()
+    obj2.setSpeed(100)
+    print("\nTest Case 2\n")
+    test_case(obj2)
+
+    # Third Test Case:
+    obj3 = Car()
+    obj3.setSpeed(-5)
+    print("\nTest Case 3\n")
+    test_case(obj3)
+
+
+if __name__ == "__main__":
+    main()
